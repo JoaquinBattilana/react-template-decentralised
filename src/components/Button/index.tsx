971b011dfeb23163a08e01ed20c21725731aa5c9
Joaquin Battilana <battilanajn@gmail.com>
2021-12-05 19:03:44 -0300

    basic testing setup

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,7 @@
-const Button = () => <button>Hola</button>
+interface Props {
+  onClick: React.MouseEventHandler<HTMLElement>
+}
+
+const Button : React.FC<Props> = ({ onClick }) => <button onClick={onClick}>Hola</button>
 
 export default Button;
